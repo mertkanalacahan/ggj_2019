b407930b1df19afe50977c71681d9ea3b698c2af
mertkanalacahan <mertkanalacahan@gmail.com>
2019-01-27 12:05:17 +0300

    almost done

---
+++ Assets/FinishGame.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Hellmade.Sound;
+
+public class FinishGame : MonoBehaviour
+{
+    public AudioClip tam_aktarma;
+    bool didPlay = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag != "isntMe" && !didPlay)
+        {
+            EazySoundManager.PlaySound(tam_aktarma, 1.0f, false, null);
+            didPlay = true;
+        }
+    }
+}
